f2f38430e65f6dd1132d72e51c24ffa4e16a5bd8
pthang23 <thangpham1324@gmail.com>
2025-02-22 14:23:14 +0700

    modify config

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     });
 
     try {
-      const response = await axios.post("http://127.0.0.1:5000/upload", formData, {
+      const response = await axios.post("https://excel-service-sc3e.onrender.com/upload", formData, {
         headers: { "Content-Type": "multipart/form-data" },
         responseType: "blob",
       });
